9e8f1cbc4118dedd4bcb73e28b88560879eb9a82
Sean Lin <sean.lin@loanmarket.com.au>
2019-06-07 22:45:06 +1000

    Modules and Packages

---
+++ pyml/utils/__init__.py
@@ -0,0 +1,2 @@
+from .plot_decision_regions import plot_decision_regions
+from .sbs import SBS
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name='pyml', version='1.0', packages=find_packages())
