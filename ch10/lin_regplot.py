cf5cc3798b2d20a44cbe27eff1a937fb2a7ff287
Sean Lin <sean.linxs@gmail.com>
2019-06-13 16:07:16 +1000

    Add housing linear regression models

---
+++ ch10/lin_regplot.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+
+def lin_regplot(X, y, model):
+    plt.scatter(X, y, c='steelblue', edgecolor='white', s=70)
+    plt.plot(X, model.predict(X), color='black', lw=2)
+    return None
